ec2a280da90c6301d9d55bd81c62786db81225f1
nakein <55964645+nakein@users.noreply.github.com>
2022-10-22 14:49:57 -0300

    Funcionamiento basico de la gema devise
    Implementado login, registro y cerrar sesion

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+#Agregada gema devise para login y registro
+gem "devise"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :clientes
+  resources :clientes
+  root "main#home"
 end
