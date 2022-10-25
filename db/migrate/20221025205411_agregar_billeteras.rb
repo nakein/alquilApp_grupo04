0ed4e2b64cf89fd150f652eeb8540a17c48e35b8
joaquin <joaquin.stella@alu.ing.unlp.edu.ar>
2022-10-25 20:16:37 -0300

    Modelo Billetera creado; se asocia a Usuario; se muestra nombre y saldo actual de usuario en home.html.erb

---
+++ db/migrate/20221025205411_agregar_billeteras.rb
@@ -0,0 +1,9 @@
+class AgregarBilleteras < ActiveRecord::Migration[7.0]
+  def change
+    create_table :billeteras do |t|
+      t.float :saldo, null: false, default: 0.0
+      t.belongs_to :usuario
+      t.timestamps null: false
+    end
+  end
+end
