581049060016c67f53565f779485403fcc4007f2
joaquin <joaquin.stella@alu.ing.unlp.edu.ar>
2022-10-27 20:00:06 -0300

    home

---
+++ app/controllers/billetera_controller.rb
@@ -0,0 +1,13 @@
+class BilleteraController < ActionController::Base
+    def mi_billetera
+    end
+
+    def get
+    end
+
+    def show
+        
+        render "mi_billetera"
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :usuarios, :controllers => {registrations: 'registrations'}
   resources :usuarios
+  resources :billetera
   root "main#home"
 end
