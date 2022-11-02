c8029f4fceeca627ca28dd1ff0827f70484cf72f
nakein <55964645+nakein@users.noreply.github.com>
2022-11-01 22:24:56 -0300

    Ahora en editar licencia al subir un formato erroneo no se pierde la licencia que tenias

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -2,9 +2,12 @@ class UsuariosController < ApplicationController
 
     def update
         @usuario = current_usuario
+        copy = @usuario.dup
+        copy.license.attach(@usuario.license.blob)
         if @usuario.update(profile_params)
           redirect_to perfil_mi_perfil_path, notice: "La licencia fue actualizada"
         else
+          @usuario.license.attach(copy.license.blob)
           render "perfil/mi_perfil"
         end
     end
