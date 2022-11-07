418f6560e2095edf867e7d82c77616e22d85aa1c
nakein <55964645+nakein@users.noreply.github.com>
2022-11-06 22:45:36 -0300

    Funcionalidad aceptar licencia agregada, helper creado, arreglos de diseño y correccion de errores

--- app/helpers/usuarios_helper.rb
+++ app/helpers/usuarios_helper.rb
@@ -1,2 +1,7 @@
 module UsuariosHelper
+    def license_validated(usuario)
+        usuario = Usuario.find(params[:id]) 
+        usuario.valid_license = !usuario.valid_license
+        usuario.save
+    end
 end
