597c084cc82769a7616a2a4927306376ffbd1ce0
braii <llamoccas.brian@gmail.com>
2022-11-07 21:32:09 -0300

    unlock_token agregado

--- db/schema.rb
+++ db/schema.rb
@@ -71,9 +71,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_011857) do
     t.date "birthdate"
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
+    t.string "unlock_token"
     t.boolean "valid_license", default: false
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_usuarios_on_unlock_token", unique: true
   end
 
   create_table "vehiculos", force: :cascade do |t|
