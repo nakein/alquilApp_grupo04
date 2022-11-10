467b97c9214733892e9ff756f69b55393956c067
nakein <55964645+nakein@users.noreply.github.com>
2022-11-10 00:06:42 -0300

    Modales agregados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_011857) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_151238) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
