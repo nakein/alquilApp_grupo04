026db1b77208738f443573ed91284bb02485ef9c
Braii <llamoccas.brian@gmail.com>
2022-11-14 23:02:32 -0300

    actualizado schema con mpvalor default en la proximidad

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_151238) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_011857) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_151238) do
     t.string "transmission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "proximity"
+    t.integer "proximity", default: 100
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
