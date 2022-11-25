4819c67c0f50115f1c76a7f0e6cb5a36cfe24300
Braii <llamoccas.brian@gmail.com>
2022-11-25 19:42:33 -0300

    implementado ordenar reportes desde supervisor

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,11 @@
 class ReportsController < ApplicationController
-    
+    $r_order = 0;
+
     def index
         if (current_usuario.supervisor?)
-            @reports=Report.all
-        else
-            @reports=Report.where(usuario_id:params[:id])
+            @reports = Report.order(sort_column + ' ' + sort_direction)
+        elsif (current_usuario.cliente?)
+            @reports=Report.where(usuario_id:current_usuario.id)
         end
     end
 
@@ -31,12 +32,13 @@ class ReportsController < ApplicationController
 
     def update
         @report = Report.find(params[:id])
-    
-        if @report.update(report_params)
-          redirect_to root_path, notice: "Se ha cambiado el estado del reporte"
-        else
-            render :edit
+        
+        if ( @report.status == "esperando" )
+            @report.status = 1
         end
+        @report.save
+        redirect_to reports_path, notice: "Se ha cambiado el estado del reporte"
+        
     end
 
     def destroy
@@ -44,7 +46,15 @@ class ReportsController < ApplicationController
 
     private
         
+    def sort_column
+        params[:sort] || "id"
+    end
+
+    def sort_direction
+        params[:direction] || "asc"
+    end
+
     def report_params
-        params.require(:report).permit(:subject, :message, :status);
+        params.require(:report).permit(:subject, :message, :status, :usuario_id);
     end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ApplicationRecord
     belongs_to:usuario
-    enum status: [:"en espera", :realizado]
+    enum status: [:esperando, :realizado]
 end
