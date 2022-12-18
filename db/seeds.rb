b46e07e5972797895c3adbb81334544829a0645a
nakein <55964645+nakein@users.noreply.github.com>
2022-12-17 21:43:20 -0300

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+usuario = Usuario.new
+usuario.email = 'admin@gmail.com'
+usuario.password = 'admin123'
+usuario.password_confirmation = 'admin123'
+usuario.role = 2
+usuario.validated = true
+usuario.save!
